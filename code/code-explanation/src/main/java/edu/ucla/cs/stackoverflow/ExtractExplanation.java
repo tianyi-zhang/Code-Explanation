3e50038b3c560fc48d2c138d019215e4aca7c006
baranyildirim <baran.yildirim@outlook.com>
2018-05-17 05:32:23 -0700

    Cleaned up code and slightly altered interface for easier debugging

--- code/code-explanation/src/main/java/edu/ucla/cs/stackoverflow/ExtractExplanation.java
+++ code/code-explanation/src/main/java/edu/ucla/cs/stackoverflow/ExtractExplanation.java
@@ -6,26 +6,32 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 
 @SuppressWarnings("resource")
 public class ExtractExplanation {
 
-	public void getPostExplanations(String input) {
+	public void getPostExplanations(String input, String output) {
 		try {		
-			
+				File o = new File(output);
+				if(o.exists()) {
+					o.delete();
+				}
+				o.createNewFile();
 			// Delimiter string depends on implementation of ExtractJavaPost
 			String delimiter = "===UCLA===";
 			
@@ -40,17 +46,44 @@ public class ExtractExplanation {
 				// Tokenize code and match tokens with sentences 
 				// Write buffer to output file 
 				
-				String post = scanner.next();
-				System.out.println(post);
-				ArrayList<String> sentences = getPostSentences(post);
-				System.out.println("Printing p tags:");
-				for(int i = 0; i < sentences.size(); i++)
-					System.out.println(sentences.get(i));
+				final Boolean DEBUG_FLAG = true;
 				
+				String post = scanner.next();
+
+				ArrayList<String> sentences = getPostSentences(post);
+
 				String code = getPostCode(post);
-				System.out.println("\nPrinting code:");
-				System.out.println(code);
-			
+
+				PartialProgramParser parser = new PartialProgramParser();
+				CompilationUnit cu = parser.getCompilationUnit(code);
+				JavaSnippetTokenizer tokenizer = new JavaSnippetTokenizer();
+				
+
+				if(tokenizer != null && cu != null) {
+					cu.accept(tokenizer);
+				}
+				
+				
+				if(DEBUG_FLAG) {
+					String postOutput = new String();
+					postOutput += post;
+					postOutput += "Sentences:\n";
+					for(int i = 0; i < sentences.size(); i++)
+						postOutput += sentences.get(i) + "\n";
+					postOutput += "\nCode:\n";
+					postOutput += code;
+					postOutput += "\nCode tokens:\n";
+					if(tokenizer != null && cu != null) {
+						postOutput += tokenizer.elements.toString();
+					}
+					else {
+						postOutput += "Java tokenizer failed for this post.\n";
+					}
+					
+					System.out.println(postOutput);
+					if(output != null)
+						FileUtils.writeStringToFile(o, postOutput, Charset.defaultCharset(), true);
+				}
 			}
 			
 			reader.close();
@@ -62,6 +95,8 @@ public class ExtractExplanation {
 		}
 		catch (IOException exception) {
 			exception.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
@@ -102,40 +137,3 @@ public class ExtractExplanation {
 		return codeTagRemover.replaceAll("");
 	}
 }
-
-
-/*
-private ArrayList<String> getPostSentences(String post) {
-	//Extract contents of p tags
-	Pattern pTagPattern = Pattern.compile("<p>(.*)</p>");
-	Matcher pTagMatcher = pTagPattern.matcher(post);
-
-	ArrayList<String> explanation = new ArrayList<String>();
-	
-	if(post == null || post.length() == 0)
-		return explanation;
-	
-	while(pTagMatcher.find()) {
-		String curr = new String(pTagMatcher.group(0));
-		curr = curr.replaceAll("<p>", "");
-		curr = curr.replaceAll("</p>", "");
-		curr = curr.replaceAll("<.*?>", "");
-		curr = curr.replaceAll("<(.+)>([^<]+)</\\\\1>", "");
-		curr = curr.replaceAll("http:.*? ", "");
-		Scanner sentenceScanner = new Scanner(curr).useDelimiter(":|\\n|\\.|\\<");
-		while(sentenceScanner.hasNext()) {
-			String token = new String();
-			token = sentenceScanner.next();	
-			int spaceCount = 0;
-			for(int i = 0; i < token.length(); i++)
-				if(token.charAt(i) == ' ')
-					spaceCount++;
-			if(spaceCount != 0 && token.length() > 1)
-				explanation.add(token.trim());
-					
-		}
-	}
-	
-	return explanation;
-}
-*/
