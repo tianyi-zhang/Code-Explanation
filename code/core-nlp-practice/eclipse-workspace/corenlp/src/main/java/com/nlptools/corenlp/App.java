d16191dc739c853e59d78d24db78c975db1377ea
lekhapatil <lppatil@berkeley.edu>
2018-10-22 16:29:24 -0700

    Add files via upload
    core-nlp-practice-CoreDocument-issue

---
+++ code/core-nlp-practice/eclipse-workspace/corenlp/src/main/java/com/nlptools/corenlp/App.java
@@ -0,0 +1,13 @@
+package com.nlptools.corenlp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
